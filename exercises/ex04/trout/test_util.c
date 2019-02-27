ba823dae11de16eb7ba77ca11a98ea42dd5c3cd8
Nick Steelman <gns126@gmail.com>
2019-02-26 19:36:08 -0500

    couple comments

--- exercises/ex04/trout/test_util.c
+++ exercises/ex04/trout/test_util.c
@@ -16,41 +16,45 @@ License: Creative Commons Attribution-ShareAlike 3.0
 
 int tests_run = 0;
 
+//Test functions to check the subtraction of two time blocks
 static char *test1() {
-    struct timeval in;
+    //Test 1000.001 - 5
+    struct timeval in;//declare structs
     struct timeval out;
+    out.tv_sec = 1000;//populate structs
     out.tv_usec = 1000;
-    in.tv_usec = 5;
-    out.tv_sec = 1000;
     in.tv_sec = 5;
-    tv_sub(&out, &in);
-    char *message = "test1 failed: tv_sub(1000, 5) should return 995";
-    mu_assert(message, out.tv_sec == 995);
+    in.tv_usec = 0;
+    tv_sub(&out, &in); //subtract
+    char *message = "test1 failed: tv_sub(1000.001, 5) should return 995";
+    mu_assert(message, out.tv_sec == 995);//test result
     return NULL;
 }
 
 static char *test2() {
+  //Test 1000 - 5.001
   struct timeval in;
   struct timeval out;
-  out.tv_usec = 50;
-  in.tv_usec = 100;
-  out.tv_sec = 50;
-  in.tv_sec = 100;
+  out.tv_sec = 1000;
+  out.tv_usec = 0;
+  in.tv_sec = 5;
+  in.tv_usec = 1000;
   tv_sub(&out, &in);
-  char *message = "test2 failed: tv_sub(50,100) should return -51";
-  mu_assert(message, out.tv_sec == -51);
+  char *message = "test2 failed: tv_sub(1000,5.001) should return 994";
+  mu_assert(message, out.tv_sec == 994);
   return NULL;
 }
 
 static char *test3() {
+  //Test 1000,01 - 1000.001
   struct timeval in;
   struct timeval out;
-  out.tv_usec = 1000;
-  in.tv_usec = 10000;
   out.tv_sec = 1000;
+  out.tv_usec = 1000;
   in.tv_sec = 10000;
+  in.tv_usec = 10000;
   tv_sub(&out, &in);
-  char *message = "test3 failed: tv_sub(1000, 10000) should return -9001";
+  char *message = "test3 failed: tv_sub(1000.0001, 10000.01) should return -9001";
   mu_assert(message, out.tv_sec == -9001);
   return NULL;
 }
